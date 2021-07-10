0e912ca0080d054e328bf3c585a8b01c4685db3e
neil <esz759486@gmail.com>
2021-07-10 23:27:35 +0800

    Fix pages/Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,12 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    Index
   </q-page>
 </template>
 
 <script lang="ts">
-import ExampleComponent from 'components/CompositionComponent.vue';
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'Index',
-  components: { ExampleComponent },
-  setup() {
-
-  }
+  name: 'PageIndex',
 });
 </script>
