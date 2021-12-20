fe7b8f2129d381fd12d89902bc50f8cc7edeac49
programzheng <programzheng@gmail.com>
2021-12-21 00:47:02 +0800

    modify: router/middlewares.ts

--- src/router/middlewares.ts
+++ src/router/middlewares.ts
@@ -3,15 +3,20 @@ import {
   NavigationGuardNext
 } from 'vue-router';
 import { LocalStorage } from 'quasar'
+import { goLanguageRepositoryApi } from 'boot/axios'
 
-export function laravelBaseAdminAuth (to:RouteLocationNormalized, from:RouteLocationNormalized, next:NavigationGuardNext):void {
+export function laravelBaseAdminAuth (to:RouteLocationNormalized, from:RouteLocationNormalized, next:NavigationGuardNext) {
 	return next({ name: 'admin.php.laravel-base.login' })
 }
 
-export function goLanguageRepositoryAuth (to:RouteLocationNormalized, from:RouteLocationNormalized, next:NavigationGuardNext):void {
+export async function goLanguageRepositoryAuth (to:RouteLocationNormalized, from:RouteLocationNormalized, next:NavigationGuardNext) {
 	const token = LocalStorage.getItem('go_language_repository_user_token')
   if(!token){
     return next({ name: 'go.language-repository.login' })
   }
+  //if jwt error to login page
+  await goLanguageRepositoryApi.post('api/v1/user/auth').catch(() => {
+    return next({ name: 'go.language-repository.login' })
+  })
   return next()
 }
