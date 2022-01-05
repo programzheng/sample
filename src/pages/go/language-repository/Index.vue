0868fcb4a37f8b60091b351c1b5c831901e1e3a8
programzheng <programzheng@gmail.com>
2022-01-06 00:52:37 +0800

    add: pages/go/language-repository/Index.vue q-card router-link to dictionary

--- src/pages/go/language-repository/Index.vue
+++ src/pages/go/language-repository/Index.vue
@@ -1,5 +1,19 @@
 <template>
-	<div class="q-pa-md">
-		language-repository
+	<div class="q-pa-md row center justify-evenly">
+		<div>
+			<router-link class="display-under-line" :to="{name: 'go.language-repository.dictionary'}">
+				<q-card>
+					<div class="q-pa-md row">
+						<q-icon class="col-12" tag="div" name="menu_book" size="3rem" />
+					</div>
+					<q-card-section>
+						<div class="text-h5">辭典</div>
+						<div class="text-caption text-grey">
+							Dictionary
+						</div>
+					</q-card-section>
+				</q-card>
+			</router-link>
+		</div>
 	</div>
 </template>
