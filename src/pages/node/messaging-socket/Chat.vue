33c1bd75ab80723773d1f1742c4995384546e94d
programzheng <programzheng@gmail.com>
2022-02-03 01:16:18 +0800

    modify: chat with user name

--- src/pages/node/messaging-socket/Chat.vue
+++ src/pages/node/messaging-socket/Chat.vue
@@ -5,9 +5,10 @@
 				<q-chat-message
 					v-for="message in messages.results"
 					:key="message.id"
-					:name="message.socket_id"
+					:name="getMessageName(message)"
 					:text='[message.message]'
-					:sent="currentSocketId === message.socket_id"
+					:sent="getMessageSent(message)"
+					:bg-color="getMessageBackgroundColor(getMessageSent(message))"
 				/>
 
 				<div>
@@ -20,21 +21,40 @@
 </template>
 
 <script lang='ts'>
-import { ref, reactive } from 'vue'
+import { onMounted, ref, reactive } from 'vue'
 import { useQuasar } from 'quasar'
 import { io, Socket } from 'socket.io-client'
-import { nodeMessagingSocketApiUserTokenKey } from 'boot/axios'
+import { nodeMessagingSocketApi, nodeMessagingSocketApiUserTokenKey } from 'boot/axios'
+
+interface User {
+	uuid: string;
+	UserProfile: UserProfile;
+}
+
+interface UserProfile {
+	userId: number;
+	email: string;
+	name: string;
+}
 
 interface Message {
 	id: number;
-	socket_id: string;
+	socketId: string;
+	userId: number;
 	message: string;
+	User: User
 }
 
 export default {
 	setup() {
 		const $q = useQuasar()
 		const userToken = $q.localStorage.getItem(nodeMessagingSocketApiUserTokenKey)
+		const userProfile = ref<UserProfile | null>(null)
+		onMounted(async () => {
+			const userProfileReponse = await nodeMessagingSocketApi.get('api/v1/user_profiles')
+			userProfile.value = userProfileReponse.data as UserProfile
+		});
+	
 		const socket: Socket = io(process.env.NODE_MESSAGING_SOCKET as string, {
 			extraHeaders: {
 				Authorization: `Bearer ${userToken as string}`
@@ -55,18 +75,43 @@ export default {
 
 		const addMessage = ref('')
 
+		/**
+		 * Functions
+		 */
+		const	getMessageName = (message:Message) => {
+			if(message.userId > 0 && message.User && message.User.UserProfile) return message.User.UserProfile.name
+
+			return message.socketId
+		}
+		const	getMessageSent = (message:Message) => {
+				if(message.userId === userProfile.value?.userId) return true
+
+				if(currentSocketId.value === message.socketId) return true
+
+				return false
+		}
+		const getMessageBackgroundColor = (sent:boolean) => {
+			return sent ? 'green-4' : 'grey-4'
+		}
+		const sendMessage = () => {
+			if(addMessage.value === '') return false
+			socket.emit('add:message', {
+				message: addMessage.value
+			})
+			addMessage.value = ''
+		}
+
 		return {
+			userProfile,
 			socket,
 			currentSocketId,
 			messages,
 			addMessage,
 
-			sendMessage() {
-				socket.emit('add:message', {
-					message: addMessage.value
-				})
-				addMessage.value = ''
-			}
+			getMessageName,
+			getMessageSent,
+			getMessageBackgroundColor,
+			sendMessage
 		}
 	}
 }
