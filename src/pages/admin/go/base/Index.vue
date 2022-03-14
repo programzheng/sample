426565914bf55a83a3c2042aefed43195090143a
programzheng <programzheng@gmail.com>
2022-03-14 22:44:45 +0800

    add: go.base.admin.index post link

--- src/pages/admin/go/base/Index.vue
+++ src/pages/admin/go/base/Index.vue
@@ -1,5 +1,19 @@
 <template>
-	<div class="q-pa-md">
-		GoBase
+	<div class="q-pa-md row center justify-evenly">
+		<div>
+			<router-link class="display-under-line" :to="{name: 'go.base.admin.post'}">
+				<q-card>
+					<div class="q-pa-md row">
+						<q-icon class="col-12" tag="div" name="menu_book" size="3rem" />
+					</div>
+					<q-card-section>
+						<div class="text-h5">文章</div>
+						<div class="text-caption text-grey">
+							Post
+						</div>
+					</q-card-section>
+				</q-card>
+			</router-link>
+		</div>
 	</div>
 </template>
